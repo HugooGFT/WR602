835f17f472fdb58d7cbb7b220b4b5f424b87b1e2
HugooGFT <hugo.gaffet@gmail.com>
2024-02-05 15:47:23 +0100

    feat: user entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -27,6 +27,21 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private ?string $password = null;
 
+    #[ORM\Column(length: 25)]
+    private ?string $firstname = null;
+
+    #[ORM\Column(length: 25)]
+    private ?string $lastname = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $created_at = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $updated_at = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $subscription_end_at = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -95,5 +110,65 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): static
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): static
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $created_at): static
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updated_at): static
+    {
+        $this->updated_at = $updated_at;
+
+        return $this;
+    }
+
+    public function getSubscriptionEndAt(): ?\DateTimeImmutable
+    {
+        return $this->subscription_end_at;
+    }
+
+    public function setSubscriptionEndAt(\DateTimeImmutable $subscription_end_at): static
+    {
+        $this->subscription_end_at = $subscription_end_at;
+
+        return $this;
     }
 }
