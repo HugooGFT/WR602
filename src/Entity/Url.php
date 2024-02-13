8ff19e145f3656769f937308d850d5b6d8625b9a
HugooGFT <hugo.gaffet@gmail.com>
2024-02-13 12:01:02 +0100

    feat/ url saved in DB

--- src/Entity/Url.php
+++ src/Entity/Url.php
@@ -13,19 +13,24 @@ class Url
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $url = null;
+
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getUrl(): string
+    public function getUrl(): ?string
     {
         return $this->url;
     }
+
     public function setUrl(string $url): static
     {
         $this->url = $url;
 
         return $this;
     }
+
 }
