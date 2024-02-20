702a2b8da49cf5bfa91334ae793a784d8b7c5c15
HugooGFT <hugo.gaffet@gmail.com>
2024-02-20 09:54:11 +0100

    feat: Register with subscription id set to 2

--- src/Controller/SubscriptionController.php
+++ src/Controller/SubscriptionController.php
@@ -2,15 +2,20 @@
 
 namespace App\Controller;
 
+use App\Entity\Subscription;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SubscriptionController extends AbstractController
 {
     #[Route('/subscription', name: 'app_subscription')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+
+        $subsciption = new Subscription();
+        $subsciption->setTitle('titre');
 
         return $this->render('subscription/index.html.twig', [
             'controller_name' => 'SubscriptionController',
